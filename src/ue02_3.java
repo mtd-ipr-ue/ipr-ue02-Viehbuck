8c967a28f96a1fdcc1a26d13865a7b4b32a8219f
david <dvhiehbock@gmail.com>
2024-10-28 13:48:37 +0100

    ue02_1, ue02_2 ue02_3 gmeacht

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -1,7 +1,20 @@
 import libraries.Out;
+import libraries.In;
 
 public class ue02_3 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        Out.print("X-Wert:");
+        double x = In.readDouble();
+        Out.print("Y-Wert:");
+        double y =In.readDouble();
+
+        double hypothenuse = Math.sqrt(x*x + y*y);
+
+        if(hypothenuse >= 4 && x <= 4 && y<=4){
+            Out.print("innerhalb der grünen Fläche");
+        }
+        else{
+            Out.print("Außerhalb der grünen Fläche");
+        }
     }
 }
