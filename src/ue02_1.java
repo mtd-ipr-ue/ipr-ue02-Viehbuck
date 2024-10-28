d8a7fb19940faf519c2a755dc01d1d9e3ee855df
david <dvhiehbock@gmail.com>
2024-10-28 13:49:35 +0100

    Übung1 ,2, 3 gmeacht

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,22 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+
+        int a = In.readInt();
+        int b = In.readInt();
+        int c = In.readInt();
+
+        int max = a;
+
+        if(max < b)
+        {
+            max = b;
+        }
+        if(max < c){
+            max = c;
+        }
+        Out.print("Das Maximum der Zahlen ist: "+max);    
     }
 }
--- src/ue02_2.java
+++ src/ue02_2.java
@@ -1,7 +1,39 @@
 import libraries.Out;
+import libraries.In;
 
 public class ue02_2 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        Out.print("Jahreszahl:");
+        int jahr = In.readInt();
+        Out.print("Monat:");
+        int monat = In.readInt();
+        int days = 0;
+
+        if(monat < 1 || monat > 12){
+            Out.print("Fehler, dises Monat gibt es nicht");
+        }
+        else{
+            switch(monat){
+                case 1, 3, 5, 7, 8, 10, 12:
+                    days = 31;
+                    break;
+                case 4, 6, 9, 11:
+                    days = 30;
+                    break;
+                case 2:
+                    if((jahr % 4 == 0 && jahr % 100 != 0) || (jahr % 400 == 0)){
+                        days = 29;
+                    }
+                    else{
+                        days=28;
+                    }
+                    break;
+                default:
+                Out.print("Fehler");
+            }
+        }
+        
+        Out.print("Das Monat hat:"+days+" Tage");
+
     }
 }
