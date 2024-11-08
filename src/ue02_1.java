847112382dd0b8f63656f11cfe9310e26812d11b
david <dvhiehbock@gmail.com>
2024-11-08 14:54:45 +0100

    finished

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -3,9 +3,11 @@ import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
-
+        Out.print("Zahl1:");
         int a = In.readInt();
+        Out.print("Zahl2:");
         int b = In.readInt();
+        Out.print("Zahl3:");
         int c = In.readInt();
 
         int max = a;
